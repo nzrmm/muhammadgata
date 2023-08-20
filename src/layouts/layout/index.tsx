37587383dcc1c15a6f68a3de48bd214c8b9e2f0c
muhammadgata <iamgata.me@gmail.com>
2023-08-20 23:23:21 +0700

    feat: add layout layouts

---
+++ src/layouts/layout/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import cx from "classnames";
+import { Space_Grotesk, Inter } from "next/font/google";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  preload: true,
+  display: "swap",
+  variable: "--font-space-grotesk",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  preload: true,
+  display: "swap",
+  variable: "--font-inter",
+});
+
+const Layout = ({ children }: React.PropsWithChildren) => {
+  return (
+    <div
+      className={cx(
+        spaceGrotesk.variable,
+        inter.variable,
+        "font-primary antialiased"
+      )}
+    >
+      <main className={cx("mx-auto lg:max-w-4xl")}>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,12 @@
 import "@/styles/globals.css";
-import cx from "classnames";
 import type { AppProps } from "next/app";
-import { Space_Grotesk, Inter } from "next/font/google";
 
-const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
-  preload: true,
-  display: "swap",
-  variable: "--font-space-grotesk",
-});
-
-const inter = Inter({
-  subsets: ["latin"],
-  preload: true,
-  display: "swap",
-  variable: "--font-inter",
-});
+import { Layout } from "@/layouts";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={cx(spaceGrotesk.variable, inter.variable, "font-primary")}>
+    <Layout>
       <Component {...pageProps} />
-    </main>
+    </Layout>
   );
 }
