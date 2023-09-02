9c3f45fa06901d378bd0e56d57325bd7c55d018b
muhammadgata <iamgata.me@gmail.com>
2023-09-03 00:28:21 +0700

    fix: change implementation next/font in root app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,42 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { Space_Grotesk, Inter } from "next/font/google";
 
 import { Layout } from "@/layouts";
 
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  preload: true,
+  display: "swap",
+  variable: "--font-space-grotesk",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  preload: true,
+  display: "swap",
+  variable: "--font-inter",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider enableSystem attribute="class" storageKey="app-theme">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <style jsx global>{`
+        :root {
+          --font-space-grotesk: ${spaceGrotesk.style.fontFamily};
+          --font-inter: ${inter.style.fontFamily};
+        }
+        html {
+          font-family: var(--font-space-grotesk);
+        }
+      `}</style>
+
+      <ThemeProvider enableSystem attribute="class" storageKey="app-theme">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   );
 }
