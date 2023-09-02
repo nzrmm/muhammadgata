346d0cdaaaf303deffc9691985d222d4236b7d57
muhammadgata <iamgata.me@gmail.com>
2023-09-02 23:42:06 +0700

    fix: remove custom-navlink component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,7 @@
 import Button from "@/components/button";
 import Typography from "@/components/typography";
-import CustomLink from "@/components/custom-link";
 import NavbarItem from "@/components/navbar-item";
 import DropdownMenu from "@/components/dropdown-menu";
 import ThemeSelector from "@/components/theme-selector";
 
-export {
-  Button,
-  Typography,
-  CustomLink,
-  NavbarItem,
-  DropdownMenu,
-  ThemeSelector,
-};
+export { Button, Typography, NavbarItem, DropdownMenu, ThemeSelector };
