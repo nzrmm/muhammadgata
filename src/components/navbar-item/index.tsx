ba1d361b6e60869ec5edc75e4420063f7e85863f
muhammadgata <iamgata.me@gmail.com>
2023-09-04 18:33:28 +0700

    feat: update implementation isActive in navbar-item component

--- src/components/navbar-item/index.tsx
+++ src/components/navbar-item/index.tsx
@@ -1,24 +1,35 @@
 import React from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import Link, { type LinkProps } from "next/link";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/utils";
 import { INavbarLink } from "@/types";
 
-const NavbarItem = ({ href, name }: INavbarLink) => {
-  const { asPath } = useRouter();
+type INavbarItemProps = LinkProps & INavbarLink;
+
+const NavbarItem = ({ href, name, ...props }: INavbarItemProps) => {
+  const pathname = usePathname();
+
+  const isActive = () => {
+    if (href === "/" && pathname === href) return true;
+    if (href !== "/" && pathname.includes(href)) return true;
+
+    return false;
+  };
 
   return (
     <Link
       href={href}
       className={cn(
-        "px-6 py-1.5 rounded-md hover:bg-neutral-100 hover:dark:bg-neutral-800",
+        "px-6 py-1.5 rounded-md",
+        "text-neutral-700 hover:bg-neutral-100",
+        "dark:text-neutral-300 hover:dark:bg-neutral-800 ",
         {
           "font-semibold bg-neutral-100 text-neutral-900 dark:bg-neutral-800 dark:text-white":
-            asPath === href,
-          "text-neutral-700 dark:text-neutral-300": asPath !== href,
+            isActive(),
         }
       )}
+      {...props}
     >
       {name}
     </Link>
