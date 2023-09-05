cb05bd74700b89adbd8412879c640734a68ed261
muhammadgata <iamgata.me@gmail.com>
2023-09-06 00:32:02 +0700

    feat: create projects page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "muhammad-gata.vercel.app",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,47 @@
+import Head from "next/head";
+
+import { Typography, ProjectCard } from "@/components";
+import { useScreenDetector } from "@/hooks";
+import { cn } from "@/utils";
+import { projects } from "@/data";
+
+const Projects = () => {
+  const { isMobile, isTablet } = useScreenDetector();
+
+  return (
+    <div>
+      <Head>
+        <title>Projects - Muhammad Gata</title>
+        <meta
+          name="description"
+          content="a collection of projects that i worked on"
+        />
+      </Head>
+
+      <div className={cn("mb-20")}>
+        <div className={cn("mb-16", "lg:mb-20")}>
+          <Typography
+            variant={isMobile || isTablet ? "text4Xl" : "text6Xl"}
+            className={cn("mb-4", "lg:mb-4")}
+          >
+            Projects
+          </Typography>
+
+          <Typography variant={isMobile || isTablet ? "textBase" : "textLg"}>
+            Welcome to my creative web corner. Here, you will discover various
+            web development projects that I have built. Every website I produce
+            combines modern aesthetics with strong functionality.
+          </Typography>
+        </div>
+
+        <div className={cn("grid gap-8", "lg:gap-16")}>
+          {projects.map((project, index) => (
+            <ProjectCard key={index} isReverse={index % 2 === 0} {...project} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
