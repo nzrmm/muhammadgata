ac2ebb15fc1c438d66927afc4e4a07997e8280e8
muhammadgata <iamgata.me@gmail.com>
2023-09-12 23:48:48 +0700

    feat: handle responsive tool card component in mobile device

--- src/components/tool-card/index.tsx
+++ src/components/tool-card/index.tsx
@@ -1,29 +1,34 @@
 import { Typography } from "@/components";
 import { cn } from "@/utils";
+import { useScreenDetector } from "@/hooks";
 import { IToolType } from "@/types/tool";
 
 type IToolsCardProps = IToolType;
 
 const ToolCard = ({ name, description, icon, icon_color }: IToolsCardProps) => {
+  const { isDesktop } = useScreenDetector();
+
   const Icon = icon;
 
   return (
     <div
       className={cn(
-        "w-full h-80 bg-white border border-neutral-900/10 rounded-lg overflow-hidden",
-        "dark:bg-neutral-900 dark:border-white/10"
+        "flex w-full bg-white border border-neutral-900/10 rounded-lg overflow-hidden",
+        "dark:bg-neutral-900 dark:border-white/10",
+        "lg:block lg:h-80"
       )}
     >
       <div
         className={cn(
-          "w-full h-40 bg-neutral-100 flex items-center justify-center",
-          "dark:bg-neutral-800"
+          "w-28 bg-neutral-100 flex items-center justify-center",
+          "dark:bg-neutral-800",
+          "lg:w-full lg:h-1/2"
         )}
       >
-        <Icon color={icon_color} size={70} />
+        <Icon color={icon_color} size={isDesktop ? 70 : 32} />
       </div>
 
-      <div className={cn("p-5")}>
+      <div className={cn("flex-1 p-4", "lg:h-1/2 lg:p-5")}>
         <Typography
           variant="textBase"
           className={cn("mb-2 font-bold", "lg:mb-2")}
